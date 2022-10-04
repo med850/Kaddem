fca1916f8aeac635bce4e43659a24e341a093774
fatmatrb <fatma.trabelsi@esprit.tn>
2022-10-04 10:44:47 +0100

    add crud

---
+++ src/main/java/com/exemple/Kaddem/Repositories/EquipeRepository.java
@@ -0,0 +1,8 @@
+package com.exemple.Kaddem.Repositories;
+
+import com.exemple.Kaddem.Entity.Equipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipeRepository extends JpaRepository<Equipe, Integer>{
+	
+}
